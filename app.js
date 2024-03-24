e42af75ef8534ad7f2f54b9995e86a41e94a5628
Ryo-Hatakeyama <19940901ryo.hatakeyama@gmail.com>
2024-03-24 16:47:21 +0900

    section6まで

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const taskRoute = require("./routes/tasks");
 const connectDB = require("./db/connect");
 require("dotenv").config();
 app.use(express.json());
+app.use(express.static("./public"));
 
 const PORT = 5000;
 
